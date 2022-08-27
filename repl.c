3d7778dec0bf00a19fac507dda54e761d82fa1f0
consla <conslaaa@proton.me>
2022-08-27 11:01:48 -0800

    feat(parser): init parser

--- repl.c
+++ repl.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "mpc.h"
+
 #ifdef _WIN32
 #include <string.h>
 
@@ -21,7 +23,47 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
+int DEBUG = 1;
+
+int get_string_l(char* str) {
+	int pos = 0;
+
+	while (str[pos] != '\0') {
+		pos++;
+	}
+
+	return pos;
+}
+
 int main(int argc, char** argv) {
+
+	mpc_parser_t* Decimal     = mpc_new("decimal");
+	mpc_parser_t* Whole       = mpc_new("whole");
+	mpc_parser_t* Number      = mpc_new("number");
+	mpc_parser_t* Operator    = mpc_new("operator");
+	mpc_parser_t* Exp         = mpc_new("exp");
+	mpc_parser_t* Lambruh     = mpc_new("lambruh");
+
+	mpca_lang(MPCA_LANG_DEFAULT,
+	    "                                                                \
+		decimal   : /-?[0-9]+\.[0-9]+/ ;                             \
+	        whole     : /-?[0-9]+/ ;                                     \
+	        number    : <decimal> | <whole> ;                            \
+		operator  : '+' | '-' | '*' | '/' | '%'                      \
+		          | \"add\" | \"sub\" | \"mul\" | \"div\" ;          \
+		exp       : <number> | '(' <operator> <exp>+ ')' ;           \
+		lambruh   : /^/<exp>+/$/ ;                                   \
+	    ", Decimal, Whole, Number, Operator, Exp, Lambruh);
+
+	// mpca_lang(MPCA_LANG_DEFAULT,
+	//     "                                                                \
+	//         number    : /-?[0-9]+/ ;                                     \
+	// 	decimal   : /-?[0-9]+\.[0-9]+/ ;                             \
+	// 	operator  : '+' | '-' | '*' | '/' | '%'                      \
+	// 	          | \"add\" | \"sub\" | \"mul\" | \"div\" ;          \
+	// 	exp       : <number> | '(' <operator> <exp>+ ')' ;           \
+	// 	lambruh   : /^/ <exp>+ /$/ ;                                 \
+	//     ", Number, Operator, Exp, Lambruh);
 
 	puts("lambruh: lambda but bruh\n");
 	
@@ -29,9 +71,26 @@ int main(int argc, char** argv) {
 		char* input = readline("lambruh=> ");
 		add_history(input);
 
-		printf("%d\n", input);
+		mpc_result_t r;
+		if (DEBUG) {
+			printf("%s\n", input);
+		}
+		if (mpc_parse("<stdin>", input, Lambruh, &r)) {
+			mpc_ast_print(r.output);
+			mpc_ast_delete(r.output);
+			
+		} else {
+			mpc_err_print(r.error);
+			mpc_err_delete(r.error);
+		}
+		if (DEBUG) {
+			fputs("\n", stdout);
+		}
+		// printf("%s\n", input);
 		free(input);
 	}
+
+	mpc_cleanup(6, Decimal, Whole, Number, Operator, Exp, Lambruh);
 
 	return 0;
 }
